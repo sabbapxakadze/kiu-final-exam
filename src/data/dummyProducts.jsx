dc325426f1270a88d6789016471379decfc6e101
sabbapxakadze <xdxsabuka@gmail.com>
2025-06-24 15:34:16 +0400

    new data

--- src/data/dummyProducts.jsx
+++ src/data/dummyProducts.jsx
@@ -25,6 +25,46 @@ const dummyProducts = [
       "https://www.winchester.ge/uploads/product/r9nDOFPcn3eqonaj9See5dOtX87Yx0Kt88CuM9Ai.jpg",
     category: "Knife",
   },
+  {
+    id: 4,
+    title: "GIRSAN MC 9 cal.9x19",
+    price: 600,
+    image:
+      "https://www.winchester.ge/uploads/product/bKtjFgUNFiijFvi4dcR53NYLK84E2rjEddLeJKwA.jpg",
+    category: "Gun",
+  },
+  {
+    id: 5,
+    title: "Magnum Combat Dagger 02GL033",
+    price: 899,
+    image:
+      "https://www.winchester.ge/uploads/product/b8Sa30LDqcEGDUgQbiNItmsRI1NvTNDDX92bSMen.jpg",
+    category: "Knife",
+  },
+  {
+    id: 6,
+    title: "Magnum Bon Appetite 01LL209",
+    price: 100000,
+    image:
+      "https://www.winchester.ge/uploads/product/bWfwnkbrkaBJgJl4AXJcKwsa1t8bA909t1TSwlaA.jpg",
+    category: "Knife",
+  },
+  {
+    id: 7,
+    title: "DIAMONDBACK DB10 .308 16''",
+    price: 1500,
+    image:
+      "https://www.winchester.ge/uploads/product/irobQbM0KtTVW3wbTRBDbjSIfXWO1v7UpmW8zqNh.jpg",
+    category: "Gun",
+  },
+  {
+    id: 8,
+    title: "Slayer",
+    price: 9999999,
+    image:
+      "https://i.pinimg.com/736x/c8/cb/bc/c8cbbce32c8061770515a014070e1da3.jpg",
+    category: "Knife",
+  },
 ];
 
 export default dummyProducts;
